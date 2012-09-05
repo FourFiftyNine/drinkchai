3feab3e8d2f4a5c8bc09ae60da1f8ea109e1d046
Anthony Sessa <sessa@fourfiftynine.com>
2012-09-04 21:58:37 -0400

    updated case sensitivity facebook

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -19,7 +19,7 @@
  * @since         CakePHP(tm) v 0.2.9
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
-App::import('Vendor', 'facebook/src/facebook');
+App::import('Vendor', 'facebook/src/Facebook');
 App::import('Model', 'Business');
 App::uses('Controller', 'Controller');
 App::uses('CakeEmail', 'Network/Email');
